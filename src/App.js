3f07974ac7bb3666aebfea24a1534cc9841a837e
George Bryant <george.s.bryant@gmail.com>
2020-12-30 15:30:17 +0000

    Show non header components at full height

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import styled from 'styled-components';
 import Header from './components/Header/Header';
 import LeftPanel from './components/LeftPanel/LeftPanel';
+const StyledBody = styled.div`
+  display: flex;
+  height: calc(100% - 75px);
+`;
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <LeftPanel/>
+      <StyledBody>
+        <LeftPanel/>
+      </StyledBody>
     </div>
   );
 }
