349c12fa364b46012f9ee358a4a9cf21ba5741b4
George Bryant <george.s.bryant@gmail.com>
2020-12-30 20:27:42 +0000

    Add basic poster component

---
+++ src/components/Poster/Poster.js
@@ -0,0 +1,13 @@
+import StyledPoster from "./Poster.style"
+
+const Poster = ({ imagePath }) => {
+    const baseImgUrl = 'http://image.tmdb.org/t/p/';
+    const backdropSize = 'w300';
+
+    return (
+        <StyledPoster>
+        </StyledPoster>
+    )
+}
+
+export default Poster;
---
+++ src/components/Poster/Poster.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledPoster = styled.div`
+    width: 300px;
+    border-radius: 20px;
+    background-color: rgba(56, 56, 56, 0.75);
+    margin: 15px;
+`;
+
+export default StyledPoster; 
