5a19292bf3e370c00ae4cb9feffeefd2bd0d8e1d
George Bryant <george.s.bryant@gmail.com>
2020-12-30 20:27:52 +0000

    Add a role tag to improve accessibility

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <StyledBody>
+      <StyledBody role="main">
         <LeftPanel/>
         <Library/>
       </StyledBody>
